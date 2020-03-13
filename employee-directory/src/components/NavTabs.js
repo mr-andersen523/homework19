ffd2fa8a536c7afda624b1fe3d4fca8d49632e78
mr-andersen523 <chasea523@gmail.com>
2020-03-12 19:50:57 -0700

    made search bar longer

--- employee-directory/src/components/NavTabs.js
+++ employee-directory/src/components/NavTabs.js
@@ -9,9 +9,9 @@ function NavTabs(props) {
   <img src="../../public/Staff-Directory.png" height="30px" width="30px" alt="" ></img>
   <a className="navbar-brand" href="/">Employee Directory</a>
 
-  <ul class="navbar-nav ml-auto">
-  <form className="form-inline my-2 my-lg-0 " >
-      <input className ="form-control mr-sm-3 my-sm-2" type="search" placeholder="Search Employees" aria-label="Search"></input>
+  <ul class="navbar-nav ml-auto ">
+  <form className="form-inline my-2 my-lg-0 px-lg-5" >
+      <input className ="form-control mr-sm-3 my-sm-2" type="search" placeholder="Search Employees" aria-label="Search" style={{width: 500}}></input>
       <button className  ="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
     </form>
     </ul>
