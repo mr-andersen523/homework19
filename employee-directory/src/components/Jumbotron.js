c6b2924f2225e029280852bddda8d0c792b3f5c1
mr-andersen523 <chasea523@gmail.com>
2020-03-16 16:57:34 -0700

    added instructional jumbotron

---
+++ employee-directory/src/components/Jumbotron.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Jumbotron(component) {
+  return (
+<div class="jumbotron">
+    <div class="container">
+    <p className="lead text-center">Filter through employess, or use search bar to get specific results. </p>
+    </div>
+</div>
+
+);
+}
+
+export default Jumbotron;
--- employee-directory/src/components/pages/Home.js
+++ employee-directory/src/components/pages/Home.js
@@ -1,8 +1,12 @@
 import React from "react";
 import NavTabs from "../NavTabs"
+import Jumbotron from "../Jumbotron"
 
 const Home = () => (
+<div>
 <NavTabs></NavTabs>
+<Jumbotron></Jumbotron>
+</div>
 );
 
 export default Home;
