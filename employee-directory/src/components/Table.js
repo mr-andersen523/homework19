dae4fb058b13335a99283bb8658db732c8724df1
mr-andersen523 <chasea523@gmail.com>
2020-03-16 17:31:46 -0700

    added table template function

---
+++ employee-directory/src/components/Table.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Table(prop) {
+return (
+    <table class="table table-striped text-center table-hover">
+<thead>
+    <tr>
+    <th scope="col">First</th>
+    <th scope="col">Last</th>
+    <th scope="col">Email</th>
+    <th scope="col">Date of Birth</th>
+
+    </tr>
+</thead>
+<tbody>
+
+</tbody>
+</table>
+
+
+);
+}
+
+export default Table;
--- employee-directory/src/components/pages/Home.js
+++ employee-directory/src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import NavTabs from "../NavTabs"
 import Jumbotron from "../Jumbotron"
+import Table from "../Table";
 
 const Home = () => (
 <div>
 <NavTabs></NavTabs>
 <Jumbotron></Jumbotron>
+<Table></Table>
 </div>
 );
 
