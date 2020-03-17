6d8b301d8f14ff92dd9dab02b52b7b73ffb54787
mr-andersen523 <chasea523@gmail.com>
2020-03-16 17:49:05 -0700

    removed bottom margin

--- employee-directory/src/components/Jumbotron.js
+++ employee-directory/src/components/Jumbotron.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Jumbotron(component) {
   return (
-<div className="jumbotron">
+<div className="jumbotron mb-0">
     <div className="container">
     <p className="lead text-center">Filter through employess, or use search bar to get specific results. </p>
     </div>
