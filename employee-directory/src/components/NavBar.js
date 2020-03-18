97ca7b5937af3448d072cead16010df48c795734
mr-andersen523 <chasea523@gmail.com>
2020-03-17 17:20:45 -0700

    Seperated search function from navbar file

--- employee-directory/src/components/NavBar.js
+++ employee-directory/src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import SearchBar from "./SearchBar";
 
 function NavBar(component) {
   return (
@@ -9,12 +10,8 @@ function NavBar(component) {
   <span><i className="far fa-address-book" style={{color:"white", fontSize:40, padding:10}}></i></span>
   <a className="navbar-brand" href="/"> Company Employee Directory</a>
 
-  <ul className="navbar-nav ml-auto ">
-  <form className="form-inline my-2 my-lg-0 px-lg-5" >
-      <input className ="form-control mr-sm-3 my-sm-2" type="search" placeholder="Search Employees" aria-label="Search" style={{width: 500}}></input>
-      <button className  ="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
-    </form>
-    </ul>
+<SearchBar />
+
 </nav>
 
   );
---
+++ employee-directory/src/components/SearchBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function SearchBar(component) {
+  return (
+<ul className="navbar-nav ml-auto ">
+<form className="form-inline my-2 my-lg-0 px-lg-5" >
+    <input className ="form-control mr-sm-3 my-sm-2" type="search" placeholder="Search Employees" aria-label="Search" style={{width: 500}}></input>
+    <button className  ="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
+</form>
+</ul>
+
+);
+}
+
+export default SearchBar;
+
