8ae56277c2fc09692456af988ecea8fe4c857f0b
mr-andersen523 <chasea523@gmail.com>
2020-03-18 00:57:53 -0700

    deleted files

--- employee-directory/src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-// Export an object containing methods we'll use for accessing the RandomUser API
-
-export default {
-getEmployees: function() {
-    return axios.get("http://localhost:3002/api/?results=200&nat=us");
-    }
-};
