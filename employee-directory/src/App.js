b5ef1da5d090e6f3e163dac161ce2da9ad862e84
mr-andersen523 <chasea523@gmail.com>
2020-03-18 01:33:27 -0700

    edited App.js and delted API file

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,6 +1,34 @@
-import React from "react";
-import Main from "./components/pages/Main";
+import React, { useEffect, useState} from "react";
+import axios from "axios";
+import PersonsContext from "./components/personsContext";
+import Table from "./components/Table";
+import NavBar from "./components/NavBar"
+import Jumbotron from "./components/Jumbotron";
 
-const App = () => <Main />;
 
-export default App;
+//Used randomuser3 folder code to help figure out how to use API to populate table
+
+function App() {
+const [personsState, setPersonsState] = useState([]);
+
+    // Axios get with setState example and state not explicitly declared
+    useEffect(() => {
+    axios.get(`http://localhost:3002/api/?results=200&nat=us`)
+        .then(res => {
+        const personsArr  = res.data.results;
+        setPersonsState(personsArr);
+        })
+    }, [] );
+    
+    return (
+    <PersonsContext.Provider value={personsState}>
+    <div>
+        <NavBar />
+        <Jumbotron />
+        <Table />
+    </div>   
+    </PersonsContext.Provider>     
+    );
+}
+
+export default App;  
