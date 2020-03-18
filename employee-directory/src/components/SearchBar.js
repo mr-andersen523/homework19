f4633b99c0e0fe0245e4ef0fc47938db6972dda0
mr-andersen523 <chasea523@gmail.com>
2020-03-18 00:53:55 -0700

    changed search button to yellow

--- employee-directory/src/components/SearchBar.js
+++ employee-directory/src/components/SearchBar.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 function SearchBar(component) {
-  return (
+return (
 <ul className="navbar-nav ml-auto ">
 <form className="form-inline my-2 my-lg-0 px-lg-5" >
-    <input className ="form-control mr-sm-3 my-sm-2" type="search" placeholder="Search Employees" aria-label="Search" style={{width: 500}}></input>
-    <button className  ="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
+    <input className = "form-control mr-sm-3 my-sm-2" id="searchBar" type="search" placeholder="Search Employees" aria-label="Search" style={{width: 500}}></input>
+    <button className  ="btn btn-outline-warning my-2 my-sm-0" type="submit">Search</button>
 </form>
 </ul>
 
